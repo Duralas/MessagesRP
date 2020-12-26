c72dae84f2e9910f2473bf9a8a243bbe03040785
Edhrendal <bvillena.git@laposte.net>
2020-12-26 19:36:37 +0100

    feat: Cache au recensement
    * Ajout d'un cache pour le recensement :
       - Rétablis le mois et la zone selon le dernier enregistrement

--- src/Form/RecenseType.php
+++ src/Form/RecenseType.php
@@ -7,12 +7,16 @@ use App\Entity\Mois;
 use App\Entity\Periode;
 use App\Entity\Zone;
 use App\Repository\PeriodeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PostSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class RecenseType extends AbstractType
 {
@@ -21,9 +25,21 @@ class RecenseType extends AbstractType
      */
     private PeriodeRepository $periodeRepository;
 
-    public function __construct(PeriodeRepository $periodeRepository)
+    /**
+     * @var CacheInterface
+     */
+    private CacheInterface $cache;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em, CacheInterface $cache)
     {
-        $this->periodeRepository = $periodeRepository;
+        $this->periodeRepository = $em->getRepository(Periode::class);
+        $this->em = $em;
+        $this->cache = $cache;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -34,12 +50,21 @@ class RecenseType extends AbstractType
         $builder
             ->add('periode', EntityType::class, array(
                 'class' => Periode::class,
-                'query_builder' => static function (PeriodeRepository $repo) {
+                'data' => $periode,
+                'query_builder' => static function (PeriodeRepository $repo) use ($periode) {
+                    if ($periode) {
+                        return $repo->getQBActive()
+                            ->orWhere('p.code = :periode')
+                            ->setParameter('periode', $periode->getCode())
+                        ;
+                    }
+
                     return $repo->getQBActive();
                 }
             ))
             ->add('mois', EntityType::class, array(
                 'class' => Mois::class,
+                'data' => $this->em->getRepository(Mois::class)->find($options['mois']),
                 'query_builder' => static function (EntityRepository $repo) use ($periode) {
                     $qb = $repo->createQueryBuilder('m')->orderBy('m.ordre');
 
@@ -55,6 +80,7 @@ class RecenseType extends AbstractType
             ))
             ->add('zone', EntityType::class, array(
                 'class' => Zone::class,
+                'data' => $this->em->getRepository(Zone::class)->find($options['zone']),
                 'group_by' => static function (Zone $zone) {
                   return $zone->getRegion() ? $zone->getRegion()->getNom() : '';
                 },
@@ -64,14 +90,38 @@ class RecenseType extends AbstractType
                 'data' => 1,
             ))
         ;
+
+        $cache = $this->cache;
+        $builder->addEventListener(FormEvents::POST_SUBMIT, static function (PostSubmitEvent $event) use ($cache) {
+            /** @var Message $message */
+            $message = $event->getData();
+
+            // Mis en cache
+            if (($mois = $message->getMois()) && ($zone = $message->getZone())) {
+                $periodeKey = $message->getPeriode() ? $message->getPeriode()->getCode() : 'periode';
+
+                $cachedMois = $cache->getItem("{$periodeKey}_mois");
+                $cachedMois->set($mois->getOrdre());
+                $cache->save($cachedMois);
+
+                $cachedZone = $cache->getItem("{$periodeKey}_zone");
+                $cachedZone->set($zone->getCode());
+                $cache->save($cachedZone);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
+        $periode = $this->periodeRepository->findActive();
+        $periodeKey = $periode ? $periode->getCode() : 'periode';
+
         $resolver->setDefaults([
             'action' => '',
             'data_class' => Message::class,
-            'periode' => $this->periodeRepository->findActive(),
+            'periode' => $periode,
+            'mois' => $this->cache->getItem("{$periodeKey}_mois")->get(),
+            'zone' => $this->cache->getItem("{$periodeKey}_zone")->get(),
         ]);
     }
 }
